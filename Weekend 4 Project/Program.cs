56bb536dfeb04d23c170ad3ffeb3e552aa722072
mlarcode <sightsnsenses@gmail.com>
2016-07-26 19:04:30 -0400

    first commit

---
+++ Weekend 4 Project/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weekend_4_Project
+{
+    //   using System;
+
+    //static class WordTools
+    //   {
+    //       static string ReverseWords(string sentence)
+    //       {
+    //           // problem 1 reverse the words that are entered in a sentence
+    //           string[] words = sentence.Split(' ');
+    //           Array.Reverse(words);
+    //           return string.Join("", words);
+
+    //           // console output:
+
+    //       }
+    //       class Program
+    //       {
+    //           static void Main()
+    //           {
+    //               const string s1 = "my heart will go on";
+    //               string rev1 = WordTools.ReverseWords(s1);
+    //               Console.WriteLine(rev1);
+    //           }
+    //       }
+
+
+    // {
+
+
+    //{
+
+}
+            
+        }
+    //}
+
+    //}
+    /// Problem 2
+    ///Two string paramenters 
+    // User name, birth month, a, an
+//     static void Main(string[] args);
+//{
+//    string userName = args[0];
+//string month = birthMonth.ToShortMonthString();
+
+//// In put name and birth month
+//string str = userName + " Is " + month{"a" + Jan, Feb, Mar, May, Jun, July, Sep, Nov, Dec} + {"an" + Apr, Aug, Oct} + "baby";
+//System.Console.WriteLine(str);
+//Console.ReadKey();
+//}
+//}
+   // }
+
+////
+////Problem 3
+////Method with 2 parameters;
+////First name lower case, Last name lower case
+////Return first letter cap; print
+//{
+// static string UppercaseWords(string value)
+//{
+//    char[] array = value.ToCharArray();
+//    // Review First Letter in string
+//    if (array.Length >= 1)
+//    {
+//        if (char.IsLower(array[0]))
+//        {
+//            array[0] = char.ToUpper(array[0]);
+//        }
+//    }
+//    // Scan through letters and uppercase first letter
+//    for (int i = 1; i < array.Length; i++)
+//    {
+//        if (array[i - 1] == ' ')
+//        {
+//            if (char.IsLower(array[i]))
+//            {
+//                array[i] = char.ToUpper(array[i]);
+//            }
+//        }
+//    }
+//    return new string(array);
+//}
+
+//static void Main()
+//{
+//    // Uppercase words in these strings.
+//    const string value1 = "melissa";
+//    const string value2 = "reid";
+
+//    // Print the uppercase strings.
+//    Console.WriteLine(UppercaseWords(value1));
+//    Console.WriteLine(UppercaseWords(value2));
+//    Console.ReadKey();
+//}
+    
+
+
+
+//Problem 4
+//Method that finds and removes random 0 in string
+//If no 0 print = This string is okay!
+//{
+//string s = "{0}";
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Text.RegularExpressions;
+//namespace ConsoleApplication6
+//{
+//    class Program
+//    {
+//        /// <summary>
+//        /// Remove digits from string.
+//        /// </summary>
+//        public static string RemoveDigits(string key)
+//        {
+//            return Regex.Replace(key, @"\d", "");
+
+//        }
+
+//        static void Main()
+//        {
+//            Console.WriteLine("Enter input");
+//            string line = Console.ReadLine();
+            
+//            string value1 = RemoveDigits(Enterinput);
+
+//            Console.WriteLine(value1 + ("This string is ok"));
+//        }
+//    }
+
+
+
+//}
+
+
+
+//}
+
+
+
+//{
+//}
